f9131a9c47b8b2212843a0d63bf5fa536ca200cb
maayan gadasi <maayan1678@gmail.com>
2020-07-29 08:57:04 +0300

    producer concumer

--- cpp/ds/ipc/msgqueue/consumer.cpp
+++ cpp/ds/ipc/msgqueue/consumer.cpp
@@ -5,12 +5,13 @@ int main() {
     int msgid = experis::OpenQueuePipe("msgqueue");
     experis::message msgRecieve;
 
-    experis::RecieveMessage(msgid, msgRecieve);
+    while(1) {
+        experis::RecieveMessage(msgid, msgRecieve);
 
-    //msgRecieve.m_msgData = msgRecieve.m_msgData + 1;
-    experis::SendMessage(msgid, msgRecieve);
-  
-    
+        std::cout << msgRecieve.m_msgData << std::endl;
+        //msgRecieve.m_msgData = msgRecieve.m_msgData + 1;
+        experis::SendMessage(msgid, msgRecieve);
+    }
 
     return 0; 
 }
--- cpp/ds/ipc/msgqueue/producer.cpp
+++ cpp/ds/ipc/msgqueue/producer.cpp
@@ -2,17 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int main() {
 
     int msgid = experis::OpenQueuePipe("msgqueue");
     experis::message msgSend, msgRecieve;
-    srand(time(NULL));
-    msgSend.m_msgData = rand();
 
-    experis::SendMessage(msgid, msgSend);
-    experis::RecieveMessage(msgid, msgRecieve);
-    experis::CheckMessage(msgRecieve, msgSend);
+    while(1) {
+        srand(time(NULL));
+        msgSend.m_msgData = rand();
+
+        std::cout << msgSend.m_msgData << std::endl;
+        sleep(1);
+        experis::SendMessage(msgid, msgSend);
+        experis::RecieveMessage(msgid, msgRecieve);
+        experis::CheckMessage(msgRecieve, msgSend);
+    }
 
     msgctl(msgid, IPC_RMID, NULL);
 
